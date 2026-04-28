083a725a0eaca513a92389d9ee771526b20e762b
Aaryan-Gautam957 <150098417+DelevporAaryan@users.noreply.github.com>
2026-04-28 08:36:25 +0530

    Update ChatBox.js

--- Task-1/frontend/src/components/ChatBox.js
+++ Task-1/frontend/src/components/ChatBox.js
@@ -15,6 +15,15 @@ function ChatBox() {
       <textarea onChange={(e) => setText(e.target.value)} />
       <br />
       <button onClick={send}>Send</button>
+
+  {data && (
+  <div style={{ marginTop: "10px", border: "1px solid #ccc", padding: "10px" }}>
+    <h4>Extracted Data</h4>
+    <p><b>Doctor:</b> {data.doctor}</p>
+    <p><b>Product:</b> {data.product}</p>
+    <p><b>Outcome:</b> {data.outcome}</p>
+  </div>
+)}
     </div>
   );
 }
