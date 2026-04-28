4680c530cc80336ccfc6b7a84aad04bc3cc25f26
Aaryan-Gautam957 <150098417+DelevporAaryan@users.noreply.github.com>
2026-04-28 08:33:28 +0530

    Add FormBox component with manual form inputs

---
+++ Task-1/frontend/src/components/FormBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FormBox() {
+  return (
+    <div>
+      <h3>Manual Form</h3>
+      <input placeholder="Doctor" /><br />
+      <input placeholder="Product" /><br />
+      <input placeholder="Outcome" /><br />
+      <button>Save</button>
+    </div>
+  );
+}
+
+export default FormBox;
